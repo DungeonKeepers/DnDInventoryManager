08856e458d9d2fb1a0033481f154815a454d02f9
mikemiksch <mikemiksch@gmail.com>
2017-04-10 10:46:19 -0700

    Built out User, Campaign, and Character classes.

--- DnDInventoryManager/DnDInventoryManager/Character.swift
+++ DnDInventoryManager/DnDInventoryManager/Character.swift
@@ -7,3 +7,18 @@
 //
 
 import Foundation
+
+class Character {
+    
+    var id = String()
+    let name : String
+    let characterURL : String
+    let avatarURL : String?
+    
+    init(json: [String : Any]) {
+        self.name = json["name"] as? String ?? "Name Unknown"
+        self.characterURL = json["character_url"] as? String ?? "No character URL found"
+        self.avatarURL = json["avatar_url"] as? String ?? nil
+    }
+    
+}
--- DnDInventoryManager/DnDInventoryManager/User.swift
+++ DnDInventoryManager/DnDInventoryManager/User.swift
@@ -7,3 +7,24 @@
 //
 
 import Foundation
+
+class User {
+    
+    let id : String
+    let username : String
+    let avatar : String?
+    let profileURL : String
+    var campaignIDs = [String]()
+    
+    init(json: [String : Any]) {
+        self.id = json["id"] as? String ?? "ID not found"
+        self.username = json["username"] as? String ?? "Username not found"
+        self.avatar = json["avatar_image_url"] as? String ?? nil
+        self.profileURL = json["profile_url"] as? String ?? "Profile URL not found"
+        for campaigns in json["campaigns"] as! Array<NSDictionary> {
+            let campaignID = campaigns["id"]
+            self.campaignIDs.append(campaignID as! String)
+        }
+    }
+    
+}
