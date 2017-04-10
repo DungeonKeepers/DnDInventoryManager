531ff82813f5ba0b82d409ee94666b1b206bb489
mikemiksch <mikemiksch@gmail.com>
2017-04-10 12:53:59 -0700

    Working on Obsidian Portal API class, but having trouble with lack of documentation.

--- DnDInventoryManager/DnDInventoryManager/ObsidianPortal.swift
+++ DnDInventoryManager/DnDInventoryManager/ObsidianPortal.swift
@@ -9,4 +9,30 @@
 import UIKit
 
 typealias ObsidianPortalOAuthCompletion = (Bool) -> ()
+typealias FetchCampaignsCompletion = ([Campaign]?) -> ()
 
+enum ObsidianPortalAuthError : Error {
+    case extractingCode
+}
+
+enum SaveOptions {
+    case userDefaults
+}
+
+let kOAuthBaseURLString = "https://www.obsidianportal.com/oauth/"
+
+class ObsidianPortal {
+    
+    private var session: URLSession
+    private var components: URLComponents
+    
+    static let shared = ObsidianPortal()
+    
+    private init() {
+        self.session = URLSession(configuration: .default)
+        self.components = URLComponents()
+        self.components.scheme = "https"
+        self.components.host = "www.obsidianportal.com"
+    }
+    
+}
