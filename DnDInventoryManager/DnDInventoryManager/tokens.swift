41b1389ce2a269756afbfe3d13a9b64d2bf78138
mikemiksch <mikemiksch@gmail.com>
2017-04-10 09:04:50 -0700

    Added security tokens to files.

--- DnDInventoryManager/DnDInventoryManager/tokens.swift
+++ DnDInventoryManager/DnDInventoryManager/tokens.swift
@@ -7,3 +7,15 @@
 //
 
 import Foundation
+
+let consumerKey = "FU3uJznSciX9EGAn1q4d"
+let consumerSecret = "gAWu4ZHGpLJqCVE3C8GaCn8L9vFXU15f0cM4RCms"
+
+
+//Request Token URL
+//https://www.obsidianportal.com/oauth/request_token
+//Access Token URL
+//https://www.obsidianportal.com/oauth/access_token
+//Authorize URL
+//https://www.obsidianportal.com/oauth/authorize
+//We support HMAC-SHA1 for OAuth signature methods.
