353a4a78ca0cba1aaf0475271cad2c20ed94fcb2
Adrian Kenepah-Martin <ackenepah@gmail.com>
2017-04-12 08:01:36 -0700

    Reconstructing Fetching Data Function Reconstructing fetching data function to pull campaigns from DB.

--- DnDInventoryManager/DnDInventoryManager/CloudKit.swift
+++ DnDInventoryManager/DnDInventoryManager/CloudKit.swift
@@ -9,50 +9,49 @@
 import UIKit
 import CloudKit
 
-typealias CampaignCompletion = ([Campaign]?)->()
-
 class CloudKit {
 
-    // MARK: - Properties
-    let container: CKContainer
-    let database: CKDatabase
+    var campaignList: Array<CKRecord> = []
     
-    // MARK: - Initializers
-    init() {
-        container = CKContainer.default()
-        database = container.publicCloudDatabase
+    override func ViewDideLoad() {
+        super.ViewDidLoad
     }
     
-    // MARK: fetchCampaignsBabiFüd
-    func fetchCampaigns(completion: @escaping CampaignCompletion) {
-        let postQuery = CKQuery(recordType: "Campaigns", predicate: NSPredicate(value: true))
+    // MARK: fetchCampaigns
+    
+    func fetchCampaigns() {
         
-        self.database.perform(postQuery, inZoneWith: nil) { (records, error) in
-            if error != nil {
-                OperationQueue.main.addOperation {
-                    completion(nil)
-                }
-                
-                if let records = records {
-                    var campaigns = [Campaign]()
-                    
-                    for record in records {
-                        let name = record.recordType
-                        if let asset = record["img"] as? CKAsset {
-                            let path = asset.fileURL.path
-                            
-                            if let image = UIImage(contentsOfFile: path) != nil {
-                                let addCampaign = Campaign(name: name, image: path)
-                                campaigns.append(addCampaign)
-                            }
-                        }
-                    }
-                    
-                    OperationQueue.main.addOperation {
-                        completion(campaigns)
-                    }
-                }
-            }
-        }
     }
+    
+//    func fetchCampaigns(completion: @escaping CampaignCompletion) {
+//        let postQuery = CKQuery(recordType: "Campaigns", predicate: NSPredicate(value: true))
+//        
+//        self.database.perform(postQuery, inZoneWith: nil) { (records, error) in
+//            if error != nil {
+//                OperationQueue.main.addOperation {
+//                    completion(nil)
+//                }
+//                
+//                if let records = records {
+//                    var campaigns = [Campaign]()
+//                    
+//                    for record in records {
+//                        let name = record.recordType
+//                        if let asset = record["img"] as? CKAsset {
+//                            let path = asset.fileURL.path
+//                            
+//                            if let image = UIImage(contentsOfFile: path) != nil {
+//                                let addCampaign = Campaign(name: name, image: path)
+//                                campaigns.append(addCampaign)
+//                            }
+//                        }
+//                    }
+//                    
+//                    OperationQueue.main.addOperation {
+//                        completion(campaigns)
+//                    }
+//                }
+//            }
+//        }
+//    }
 }
